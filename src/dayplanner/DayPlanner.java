254bbf2d075cb2ab7ed2fb8d786dee2ffe710b3f
Griffin Scott <griffinscott@Griffins-MacBook-Air.local>
2016-03-05 19:34:47 -0800

    DayPlanner

---
+++ src/dayplanner/DayPlanner.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dayplanner; 
+
+
+public class DayPlanner {
+    private String month, message;
+        private int day, hour, minute;
+    
+    public class Appointment {
+        
+        
+        
+        public Appointment(){
+            day = 1;
+            hour = 1;
+            minute = 1;
+            month = "Jan";
+            message = "Default";
+        }
+        public Appointment (int day, int hour, int minute, String month, String message){
+            
+        }
+        
+        public String getMonth(){
+            return NewClass.getString();
+        }
+        
+        public void setMonth(int min, int max, String s){
+            if (s.length() >= min && s.length() <= max)
+                month = s; 
+        }
+        
+        public String getMessage(){
+            return NewClass.getString(0, 45);
+        }
+        
+        public void setMessage(int min, int max, String s) {
+            if (s.length() >= min && s.length() <= max)
+                message = s;             
+        }
+        
+        public int getDay(){
+            return NewClass.getInt();
+        }
+        public void setDay(int x){
+            if (x >= 1 && x <= 31) 
+		day = x; 
+            else 
+		System.out.println("Invalid day value.");
+        }
+        
+        public int getHour(){
+            return NewClass.getInt();
+        }
+        
+        public void setHour(int x){
+            if (x >= 1 && x <= 24)
+                hour = x;
+            else 
+                System.out.println("Invalid hour value.");
+        }
+        
+        public int getMinute(){
+            return NewClass.getInt();
+        }
+        
+        public void setMinute(int x){
+            if (x >= 0 && x <= 60)
+                minute = x; 
+            
+            else 
+                System.out.println("Invalid minute value.");
+        }
+        public void toString(String month, String message, int day, int hour, int minute){
+            System.out.println("Your appointment for " + message + " is on " + month + " " + day + " at " + hour + ":" + minute);
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.println ("fuck this" + month);
+    }
+       
+} 
+    
+
---
+++ src/dayplanner/NewClass.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dayplanner;
+
+import java.util.Scanner;
+
+
+public class NewClass {
+    
+        static Scanner scanner = new Scanner(System.in);
+
+
+    public static int getInt (){
+        System.out.print("Integer input: ");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static int getInt(int min, int max){
+        boolean userValue = true; 
+        int x;
+        do {
+            System.out.print("Enter an integer between " + min + " and " + max + ". ");
+            x = getInt();
+            if (x >= min && x <= max){
+                userValue = true;
+            }
+            else {
+                System.out.println ("Error. Input must be between " + min + " and " + " " + max + ". "); 
+                userValue = false; 
+            }
+        } while (!userValue); 
+        return x;
+                 
+    }
+
+    public static char getChar(){
+        System.out.print("Integer input: ");
+        return scanner.next().charAt(0);
+
+    }
+    
+    public static char getChar(char min, char max) {
+        min = Character.toUpperCase(min);
+        max = Character.toUpperCase(max);
+        boolean userValue = true;
+        char x;
+        do{
+            System.out.println("Please input a character between " + min + " and " + max + ". ");
+            x = getChar();
+            char temp = x;
+            temp = Character.toUpperCase(temp);
+            if (temp >= min && temp <= max){
+                userValue = true; 
+            }
+            else {
+                System.out.println ("Error. Input must be between " + min + " and " + max + ". ");
+                userValue = false;
+            }
+        } while (!userValue);
+        return x;
+
+    }
+    
+    public static double getDouble(){
+        System.out.print ("Double input: ");
+        return Double.parseDouble(scanner.nextLine());
+    }
+    
+    public static double getDouble(double min, double max) {
+        boolean userValue = true; 
+        double x;
+        do {
+            System.out.print("Enter an double value between " + min + " and " + max + ". ");
+            x = getDouble();
+            if (x >= min && x <= max){
+                userValue = true;
+            }
+            else {
+                System.out.println ("Error. Input must be between " + min + " and " + " " + max + ". "); 
+                userValue = false; 
+            }
+        } while (!userValue); 
+        return x;
+    }
+    
+    public static String getString(){
+        return scanner.nextLine();
+
+    }
+
+    public static String getString(int min, int max){
+        boolean userValue = true; 
+        String x;
+        do {
+            System.out.print("Enter an String between " + min + " and " + max + " characters. ");
+            x = getString();
+            if (x.length() >= min && x.length() <= max){
+                userValue = true;
+            }
+            else {
+                System.out.println ("Error. Input must be between " + min + " and " + " " + max + " characters. "); 
+                userValue = false; 
+            }
+        } while (!userValue); 
+        return x;
+         
+    }
+    
+  public static void main(String args[]) {
+    
+      System.out.println(getInt(0, 30));
+      
+      System.out.println(getChar('a', 'z'));
+      
+      System.out.println(getDouble(0.0, 30.0));
+      
+      System.out.println(getString(0, 40));
+  }
+}
+    
